829902a3ee3386be369824b32a93afd9be88565d
Prasanna Paithankar <paithankarprasanna@gmail.com>
2024-04-24 20:41:40 +0530

    updates

--- reconfigure.bash
+++ reconfigure.bash
@@ -1,5 +1,9 @@
+#!/bin/bash
 mkdir -p pkg
 cp -r Main/_site/* pkg/
 cp -r AT60205/ pkg/
-# sftp prasannabp@cse.iitkgp.ac.in/ 
-# password rAfETI0o3A6j
+
+sshpass -p "rAfETI0o3A6j" sftp prasannabp@cse.iitkgp.ac.in <<EOF
+cd public_html
+put -r pkg/*
+EOF
